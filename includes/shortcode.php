9103709fece8971566436e113475225cc9a998a6
Andrea Debernardi <andrea@debbaweb.it>
2024-08-05 09:54:39 +0200

    react app - default atts

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -4,9 +4,14 @@
 defined('ABSPATH') or die('Direct script access disallowed.');
 
 add_shortcode('alkw_widget', function ($atts) {
-    $default_atts = array();
+    $default_atts = array(
+        "lang" => substr(get_locale(), 0, 2),
+        "hide_copyright" => false
+    );
     $args = shortcode_atts($default_atts, $atts);
-    return "<style>
+
+    return "<script>var alkwp = " . json_encode($args) . ";</script><style>
 .alkw-root {width: 100%;height: 90rem;position: relative; overflow: scroll}
 @media screen and (min-device-width: 1200px)@media screen and (min-width: 48em) {.alkw-root {overflow: inherit !important;}} {.alkw-root {width: 1020px;height: 70rem;position: relative;}}</style><div id='alkw-root' class='alkw-root'></div>";
+
 });
